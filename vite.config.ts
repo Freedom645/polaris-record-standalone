ada93c7714b59fc9cbcb0b6a06ae69a501279152
Freedom645 <16729421+Freedom645@users.noreply.github.com>
2025-08-06 11:25:29 +0900

    vite-plugin-pwaの導入と適用

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,47 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      devOptions: { enabled: true },
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      injectRegister: "auto",
+      manifest: {
+        name: "Polaris Record",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "images/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "images/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "images/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "images/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/polaris-record-standalone",
 });
