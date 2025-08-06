9ddadf73b3b8bceea72dd14d8e0fbab87b010fb0
Freedom645 <16729421+Freedom645@users.noreply.github.com>
2025-08-06 11:45:07 +0900

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/polaris-record-standalone/sw.js', { scope: '/polaris-record-standalone/' })})}
